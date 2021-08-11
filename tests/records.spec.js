e06c426338a7ad31a1ddd4bd4e2c965abfbb6a6a
Amal Francis <amalfra@gmail.com>
2021-08-11 21:08:10 +0530

    add integration tests for queries

--- tests/records.spec.js
+++ tests/records.spec.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import request from 'supertest'
 
 import createApp from '../src/app.js'
+import RecordModel from '../src/models/record.js'
 import doSetup from './setup.js'
 
 describe('Records route', function() {
@@ -26,6 +27,10 @@ describe('Records route', function() {
     if (db) {
       await db.stop()
     }
+  })
+
+  afterEach(async function() {
+    await RecordModel.deleteMany({})
   })
 
   it('should have 422 for requests without any params', async function() {
@@ -118,5 +123,98 @@ describe('Records route', function() {
     expect(res.body.code).to.eq(422)
     expect(res.body.msg).to.eq('"startDate" must be less than "ref:endDate"')
     expect(res.statusCode).to.equal(422)
+  })
+
+  it('should return correct results when there are matches', async function() {
+    const records = [
+      {
+        key: 'TAKwGc6Jr4i8Z487',
+        createdAt: '2017-01-28T01:22:14.398Z',
+        counts: [150, 160],
+        value: 'Getir Task',
+      },
+      {
+        key: 'TAKwGc6Jr4i8Z487',
+        createdAt: '2017-01-28T01:22:14.398Z',
+        counts: [170],
+        value: 'Getir Task',
+      },
+      {
+        key: 'TAKwGdfgdgJr4i8Z487',
+        createdAt: '2021-01-28T01:22:14.398Z',
+        counts: [200, 300, 500],
+        value: 'Getir Task',
+      },
+    ]
+    await RecordModel.insertMany(records)
+    const expectedRecords = [
+      {
+        createdAt: '2017-01-28T01:22:14.398Z',
+        key: 'TAKwGc6Jr4i8Z487',
+        totalCount: 310,
+      },
+      {
+        createdAt: '2017-01-28T01:22:14.398Z',
+        key: 'TAKwGc6Jr4i8Z487',
+        totalCount: 170,
+      },
+    ]
+
+    const payload = {
+      startDate: '2016-01-26',
+      endDate: '2019-02-02',
+      minCount: 100,
+      maxCount: 2300,
+    }
+    const res = await request(app)
+      .post('/v1/records')
+      .set({ 'Content-Type': 'application/json' })
+      .send(payload)
+
+    expect(res.body.code).to.eq(0)
+    expect(res.body.msg).to.eq('Success')
+    expect(res.body.records).to.deep.equal(expectedRecords)
+    expect(res.statusCode).to.equal(200)
+  })
+
+  it('should return correct results when there are no matches', async function() {
+    const records = [
+      {
+        key: 'TAKwGc6Jr4i8Z487',
+        createdAt: '2017-01-28T01:22:14.398Z',
+        counts: [150, 160],
+        value: 'Getir Task',
+      },
+      {
+        key: 'TAKwGc6Jr4i8Z487',
+        createdAt: '2017-01-28T01:22:14.398Z',
+        counts: [170],
+        value: 'Getir Task',
+      },
+      {
+        key: 'TAKwGdfgdgJr4i8Z487',
+        createdAt: '2021-01-28T01:22:14.398Z',
+        counts: [200, 300, 500],
+        value: 'Getir Task',
+      },
+    ]
+    await RecordModel.insertMany(records)
+    const expectedRecords = []
+
+    const payload = {
+      startDate: '2026-01-26',
+      endDate: '2039-02-02',
+      minCount: 100,
+      maxCount: 2300,
+    }
+    const res = await request(app)
+      .post('/v1/records')
+      .set({ 'Content-Type': 'application/json' })
+      .send(payload)
+
+    expect(res.body.code).to.eq(0)
+    expect(res.body.msg).to.eq('Success')
+    expect(res.body.records).to.deep.equal(expectedRecords)
+    expect(res.statusCode).to.equal(200)
   })
 })
